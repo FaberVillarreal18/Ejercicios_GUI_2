13b8fe9e80f06881378311cb6ce5f46f7a63f46f
FaberVillarreal18 <faber.villarrealantequera@unicolombo.edu.co>
2024-04-19 15:08:41 -0500

    Guia_2

---
+++ Guia_2/src/guia_2/Guia_2.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package guia_2;
+
+import gui.Ventana;
+
+/**
+ *
+ * @author Estudiantes
+ */
+public class Guia_2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Ventana v = new Ventana();
+        v.setVisible(true);
+    }
+    
+}
